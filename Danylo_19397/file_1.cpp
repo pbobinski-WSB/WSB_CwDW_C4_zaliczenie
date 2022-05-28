50ae4500d6bf8e79723ec39a27411b31b49ddda1
Musubia <sobchukdanil@gmail.com>
2022-05-28 21:29:30 +0200

    first

---
+++ Danylo_19397/file_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+	cout << "Goodbye World!";
+
+	return 0;
+}
---
+++ Danylo_19397/file_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+	cout << "Hello cruel World!";
+
+	return 0;
+}
